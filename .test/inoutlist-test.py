6fe7517ee721bd7d36317d05422b837aa24214aa
ejtorre <ej.torre.nino@gmail.com>
2025-08-25 23:21:07 +0200

    Aplicar cambios de .gitignore

--- .test/inoutlist-test.py
+++
@@ -1,4 +0,0 @@
-from inoutlists import load, LoaderUNXML
-
-UN_sanctions_URL = "https://scsanctions.un.org/resources/xml/en/consolidated.xml"
-UN_sanctions = load(UN_sanctions_URL, loader=LoaderUNXML, description="UN sanctions list")
